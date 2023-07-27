b45c0e3717c98b10273661f7232a357e8490b662
AshrafRahul <m.ashraful.islam007@gmail.com>
2023-07-27 09:54:36 +0600

    problem solving

---
+++ 2167.cpp
@@ -0,0 +1,18 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+    int n;
+    cin>>n;
+    int a[n];
+    for(int i=0; i<n; i++) cin>>a[i];
+
+    for(int i=0; i<n-1; i++){
+        if(a[i+1]<a[i]){
+            cout<<i+2<<endl;
+            return 0;
+        }
+    }
+    cout<<0<<endl;
+
+    return 0;
+}
